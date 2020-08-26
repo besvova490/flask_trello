cded70958b4192fda99e26db71b2f2fc3a9bcd75
besova490 <besvova490@gmail.com>
2020-08-26 15:59:42 +0300

    w

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     APPLICATION_ROOT = '/api'
